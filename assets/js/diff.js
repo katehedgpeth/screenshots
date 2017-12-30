0d9380951ed88a5b0f8cd411cec306d9259d8240
Kate Hedgpeth <katehedgpeth@gmail.com>
2017-12-30 12:42:51 -0500

    clickable results layout

--- assets/js/diff.js
+++ assets/js/diff.js
@@ -1,4 +1,5 @@
 import resemble from "resemblejs";
+import {toggle_image_width} from "./socket.js";
 
 export function run_diff(img_name) {
   const row_container = document.getElementById(img_name);
@@ -6,6 +7,8 @@ export function run_diff(img_name) {
     resemble("ref/" + img_name + ".png").compareTo("test/" + img_name + ".png").onComplete(function(data) {
       const diff_container = document.getElementById(img_name + "--diff")
       diff_container.classList.remove("image--loading");
+      diff_container.style.flexGrow = "1";
+      diff_container.addEventListener("click", toggle_image_width);
       if (data.rawMisMatchPercentage > 0) {
         console.log("FAILED", img_name, data);
         var diff_image = new Image();
